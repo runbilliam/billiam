7efa51ed1fede0855237fff32e4ebfb803a19ac9
Bojan Zivanovic <bojanz@gmail.com>
2020-08-22 18:17:54 +0200

    Add a timezone pkg since stdlib doesn't support listing timezones.

---
+++ pkg/timezone/timezone.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 Bojan Zivanovic and contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package timezone
+
+import (
+	"sort"
+)
+
+// GetNames returns a list of timezone names.
+func GetNames() []string {
+	return names
+}
+
+// IsValid checks whether a timezone name is valid.
+func IsValid(name string) bool {
+	if name == "" {
+		return false
+	}
+	i := sort.SearchStrings(names, name)
+	if i >= len(names) {
+		return false
+	}
+
+	return names[i] == name
+}
---
+++ pkg/timezone/timezone_test.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2020 Bojan Zivanovic and contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package timezone_test
+
+import (
+	"testing"
+
+	"github.com/runbilliam/billiam/pkg/timezone"
+)
+
+func TestGetNames(t *testing.T) {
+	names := timezone.GetNames()
+	var got [3]string
+	copy(got[:], names[0:3])
+	want := [3]string{"Africa/Abidjan", "Africa/Accra", "Africa/Algiers"}
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestIsValid(t *testing.T) {
+	tests := []struct {
+		name string
+		want bool
+	}{
+		{"", false},
+		{"INVALID", false},
+		{"europe/belgrade", false},
+		{"Europe/Belgrade", true},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := timezone.IsValid(tt.name)
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
